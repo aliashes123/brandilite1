ded0b5b6863aeb593dbbf2441a7d6a16cd9e0e12
aliashes123 <aliashhere@gmail.com>
2024-10-16 20:51:21 -0400

    Create App.js

---
+++ public/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Chat from "./components/Chat";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Chat />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
